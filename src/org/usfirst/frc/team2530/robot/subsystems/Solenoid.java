fc9fe8db33b5b17c0963947f8e899856a87d09ef
Peter Marshall <pwmarshall@gmail.com>
2018-11-27 20:09:09 -0600

    beginning solenoids (dont use)
    DOES NOT WORK

---
+++ src/org/usfirst/frc/team2530/robot/subsystems/Solenoid.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team2530.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Solenoid extends Subsystem {
+	private Solenoid Solenoid1;
+    private Solenoid Solenoid2;
+	
+	public Solenoid(int port1, int port2) {
+		Solenoid1 = new Solenoid(8, port1);
+		Solenoid2 = new Solenoid(8, port2);
+    }
+	
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+	
+	public void extend() {
+		Solenoid1.set(true);
+		Solenoid2.set(false);
+    }
+
+    public void retract() {
+    	Solenoid1.set(false);
+    	Solenoid2.set(true);
+    }
+    
+    public boolean getSol1() {
+    	return Solenoid1.get();
+    }
+    
+    public boolean getSol2() {
+    	return Solenoid2.get();
+    }
+}
+
